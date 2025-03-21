f2fa406face178464a7fec25b6b6d8f38019e7ef
lje00220 <155710708+lje00220@users.noreply.github.com>
2025-03-22 00:55:58 +0900

    feat:#17 시간 선택 컴포넌트 생성

---
+++ src/components/features/reserve/TimeFunnel.tsx
@@ -0,0 +1,45 @@
+'use client';
+
+import { useState } from 'react';
+import CardContainer from '@/components/layout/CardContainer';
+import { Button } from '@/components/ui/button';
+import {
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+import { Input } from '@/components/ui/input';
+
+interface Props {
+  onNext: (time: string) => void;
+  onPrev: () => void;
+}
+
+const TimeFunnel = ({ onNext, onPrev }: Props) => {
+  const [time, setTime] = useState('');
+  return (
+    <CardContainer>
+      <CardHeader className='flex items-center justify-center pb-4'>
+        <CardTitle className='text-xl'>
+          원하는 예약 시간을 선택해주세요.
+        </CardTitle>
+      </CardHeader>
+      {/* CardContent는 임시 데이터입니다. */}
+      <CardContent className='my-5 flex h-3/5 items-center justify-center'>
+        <Input
+          id='time'
+          type='time'
+          value={time}
+          onChange={(e) => setTime(e.target.value)}
+        />
+      </CardContent>
+      <CardFooter className='flex justify-evenly'>
+        <Button onClick={() => onPrev()}>이전으로</Button>
+        <Button onClick={() => onNext(time)}>다음으로</Button>
+      </CardFooter>
+    </CardContainer>
+  );
+};
+
+export default TimeFunnel;
