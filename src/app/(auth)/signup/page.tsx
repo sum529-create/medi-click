76448ad0a2270feead56bd734b69c724f20b2960
sharet9446 <spees97@naver.com>
2025-03-21 20:06:51 +0900

    style:#15 SignUpPage 컴포넌트 구성

---
+++ src/app/(auth)/signup/page.tsx
@@ -0,0 +1,26 @@
+import Link from 'next/link';
+import React from 'react';
+import AuthForm from '@/components/features/auth-form';
+import { PATH } from '@/constants/routerPath';
+
+const SignUpPage = () => {
+  return (
+    <div className='w-full max-w-md'>
+      <div className='rounded-lg bg-white p-8'>
+        <h2 className='mb-8 text-center text-xl font-medium'>회원가입</h2>
+
+        <AuthForm mode='signup' />
+
+        <div className='mt-4 text-center text-sm'>
+          <span>이미 계정이 있으신가요? </span>
+
+          <Link href={PATH.LOGIN} className='text-main'>
+            로그인하러가기
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SignUpPage;
