323eeb90c207ef8aac786143ad52b8f9cb91f240
sharet9446 <spees97@naver.com>
2025-03-21 19:09:03 +0900

    refactor:#6 Link 부분을 login.tsx로 이동하여 더욱 더 직관적인 AuthForm 컴포넌트로 만듬

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import AuthForm from '@/components/features/auth-form';
 
 const LoginPage = () => {
@@ -7,6 +8,14 @@ const LoginPage = () => {
         <h2 className='mb-8 text-center text-xl font-medium'>로그인</h2>
 
         <AuthForm mode='login' />
+
+        <div className='mt-4 text-center text-sm'>
+          <span>아직 계정이 없으신가요? </span>
+
+          <Link href='/signup' className='text-[#71C4EF] hover:text-[#1399DF]'>
+            회원가입하러가기
+          </Link>
+        </div>
       </div>
     </div>
   );
--- src/components/features/auth-form.tsx
+++ src/components/features/auth-form.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import Link from 'next/link';
 import React, { useState } from 'react';
 import { Button } from '../ui/button';
 import { Input } from '../ui/input';
@@ -21,44 +20,35 @@ const AuthForm = ({ mode }: Props) => {
   };
 
   return (
-    <>
-      <form className='space-y-4'>
-        <div>
-          <Input
-            name='id'
-            type='text'
-            placeholder='아이디'
-            value={formData.id}
-            onChange={handleChange}
-            className='bg-[#F1F3F5]'
-            required
-          />
-        </div>
-
-        <div>
-          <Input
-            name='password'
-            type='password'
-            placeholder='비밀번호'
-            value={formData.password}
-            onChange={handleChange}
-            className='bg-[#F1F3F5]'
-            required
-          />
-        </div>
-
-        <Button className='w-full bg-[#71C4EF] text-white hover:bg-[#1399DF]'>
-          로그인
-        </Button>
-      </form>
-      <div className='mt-4 text-center text-sm'>
-        <span>아직 계정이 없으신가요? </span>
-
-        <Link href='/signup' className='text-[#71C4EF] hover:text-[#1399DF]'>
-          회원가입하러가기
-        </Link>
+    <form className='space-y-4'>
+      <div>
+        <Input
+          name='id'
+          type='text'
+          placeholder='아이디'
+          value={formData.id}
+          onChange={handleChange}
+          className='bg-[#F1F3F5]'
+          required
+        />
       </div>
-    </>
+
+      <div>
+        <Input
+          name='password'
+          type='password'
+          placeholder='비밀번호'
+          value={formData.password}
+          onChange={handleChange}
+          className='bg-[#F1F3F5]'
+          required
+        />
+      </div>
+
+      <Button className='w-full bg-[#71C4EF] text-white hover:bg-[#1399DF]'>
+        로그인
+      </Button>
+    </form>
   );
 };
 
