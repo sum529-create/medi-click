261660ea67b398d38c4f438acd32d71a6f7a95c7
lje00220 <155710708+lje00220@users.noreply.github.com>
2025-03-22 00:30:34 +0900

    feat:#17 타입 설정 파일 생성

---
+++ src/types/context.ts
@@ -0,0 +1,3 @@
+export type date = { date?: string; time?: string; other?: unknown };
+export type time = { date: string; time?: string; other?: unknown };
+export type other = { date: string; time: string; other?: unknown };
