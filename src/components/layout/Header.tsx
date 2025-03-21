fb7168fbe2499edc4962d9d3d20ea64659957950
Minjo Park <consistent.312@gmail.com>
2025-03-21 17:40:37 +0900

    style: heigth값 수정

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -4,7 +4,7 @@ import Nav from './Nav';
 
 const Header = () => {
   return (
-    <header className='flex h-[60px] items-center justify-between px-6 md:px-8'>
+    <header className='flex h-[80px] items-center justify-between px-6 md:px-8'>
       <Link href={PATH.HOME} className='font-bold hover:text-main'>
         MEDICLICK
       </Link>
