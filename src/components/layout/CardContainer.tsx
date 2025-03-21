e4dc8bb115535c8e6b32768308e84b12a7f41d81
lje00220 <155710708+lje00220@users.noreply.github.com>
2025-03-22 00:56:49 +0900

    feat:#17 공통 레이아웃 설정

---
+++ src/components/layout/CardContainer.tsx
@@ -0,0 +1,13 @@
+import { Card } from '../ui/card';
+
+const CardContainer = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className='flex h-screen w-full justify-center bg-gray02 pt-10'>
+      <Card className='h-[600px] w-[650px] rounded-lg p-6 shadow-lg'>
+        {children}
+      </Card>
+    </div>
+  );
+};
+
+export default CardContainer;
