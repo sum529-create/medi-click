6065cacc545c90d6966e702017b0aa335ceb6883
lje00220 <155710708+lje00220@users.noreply.github.com>
2025-03-21 18:45:45 +0900

    feat:#1 3개의 카드 컴포넌트 생성

--- src/app/(reservation)/reserve/page.tsx
+++ src/app/(reservation)/reserve/page.tsx
@@ -1,15 +1,16 @@
-import { Card, CardContent } from '@/components/ui/card';
+import CalendarCard from '@/components/features/reserve/CalendarCard';
+import FormCard from '@/components/features/reserve/FormCard';
+import ReserveTimeCard from '@/components/features/reserve/ReserveTimeCard';
+import CardContainer from '@/components/layout/CardContainer';
 import {
   Carousel,
   CarouselContent,
-  CarouselItem,
   CarouselNext,
   CarouselPrevious,
 } from '@/components/ui/carousel';
 
 /**
  * 예약 페이지
- *
  */
 
 const ReservationPage = () => {
@@ -17,27 +18,15 @@ const ReservationPage = () => {
     <div className='flex h-screen w-screen items-center justify-center bg-gray01'>
       <Carousel className='w-3/5'>
         <CarouselContent>
-          <CarouselItem>
-            <Card>
-              <CardContent className='flex aspect-square items-center justify-center p-6'>
-                <span className='text-4xl font-semibold'>1</span>
-              </CardContent>
-            </Card>
-          </CarouselItem>
-          <CarouselItem>
-            <Card>
-              <CardContent className='flex aspect-square items-center justify-center p-6'>
-                <span className='text-4xl font-semibold'>2</span>
-              </CardContent>
-            </Card>
-          </CarouselItem>
-          <CarouselItem>
-            <Card>
-              <CardContent className='flex aspect-square items-center justify-center p-6'>
-                <span className='text-4xl font-semibold'>3</span>
-              </CardContent>
-            </Card>
-          </CarouselItem>
+          <CardContainer>
+            <CalendarCard />
+          </CardContainer>
+          <CardContainer>
+            <ReserveTimeCard />
+          </CardContainer>
+          <CardContainer>
+            <FormCard />
+          </CardContainer>
         </CarouselContent>
         <CarouselPrevious />
         <CarouselNext />
