261ba614063d961dfcf7ff9b2cd9f70e5be480c1
Minjo Park <consistent.312@gmail.com>
2025-03-21 18:28:51 +0900

    style:#7 배너창 구현

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
-import React from 'react';
+import Banner from '@/components/features/home/Banner';
 
 export default function Home() {
-  return <div>HOME</div>;
+  return (
+    <div className='w-full px-20'>
+      <Banner />
+    </div>
+  );
 }
