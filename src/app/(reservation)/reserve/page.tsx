d40465911d3d077e32ce5bc9dcd2cc45f7c6995b
lje00220 <155710708+lje00220@users.noreply.github.com>
2025-03-21 16:23:20 +0900

    add:#1 예약 페이지 생성 및 Carousel 세팅

---
+++ src/app/(reservation)/reserve/page.tsx
@@ -0,0 +1,44 @@
+import { Card, CardContent } from '@/components/ui/card';
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from '@/components/ui/carousel';
+
+const ReservationPage = () => {
+  return (
+    <div className='flex h-screen items-center justify-center bg-gray01'>
+      <Carousel>
+        <CarouselContent>
+          <CarouselItem>
+            <Card>
+              <CardContent className='flex aspect-square items-center justify-center p-6'>
+                <span className='text-4xl font-semibold'>1</span>
+              </CardContent>
+            </Card>
+          </CarouselItem>
+          <CarouselItem>
+            <Card>
+              <CardContent className='flex aspect-square items-center justify-center p-6'>
+                <span className='text-4xl font-semibold'>1</span>
+              </CardContent>
+            </Card>
+          </CarouselItem>
+          <CarouselItem>
+            <Card>
+              <CardContent className='flex aspect-square items-center justify-center p-6'>
+                <span className='text-4xl font-semibold'>1</span>
+              </CardContent>
+            </Card>
+          </CarouselItem>
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
+    </div>
+  );
+};
+
+export default ReservationPage;
