8cfb317d368505906d4a5eec81ed60e2395282c4
lje00220 <155710708+lje00220@users.noreply.github.com>
2025-03-23 03:53:03 +0900

    style:#33 시간 선택 페이지 레이아웃 구현

--- src/components/features/reserve/TimeFunnel.tsx
+++ src/components/features/reserve/TimeFunnel.tsx
@@ -9,7 +9,7 @@ import {
   CardHeader,
   CardTitle,
 } from '@/components/ui/card';
-import { Input } from '@/components/ui/input';
+// import { Input } from '@/components/ui/input';
 
 interface Props {
   date: string;
@@ -18,8 +18,30 @@ interface Props {
   onPrev: (date: string) => void;
 }
 
+const morning = [
+  '09:00',
+  '09:30',
+  '10:00',
+  '10:30',
+  '11:00',
+  '11:30',
+  '12:00',
+  '12:30',
+];
+const afternoon = [
+  '14:00',
+  '14:30',
+  '15:00',
+  '15:30',
+  '16:00',
+  '16:30',
+  '17:00',
+  '17:30',
+];
+
 const TimeFunnel = ({ date, time, onNext, onPrev }: Props) => {
   const [selectedTime, setSelectedTime] = useState(time);
+  console.log(selectedTime);
 
   return (
     <CardContainer>
@@ -28,16 +50,39 @@ const TimeFunnel = ({ date, time, onNext, onPrev }: Props) => {
           원하는 예약 시간을 선택해주세요.
         </CardTitle>
       </CardHeader>
-      {/* CardContent는 임시 데이터입니다. */}
-      <CardContent className='my-5 flex h-3/5 items-center justify-center'>
-        <Input
-          id='time'
-          type='time'
-          value={selectedTime}
-          onChange={(e) => setSelectedTime(e.target.value)}
-        />
+      <CardContent className='my-5 flex h-fit flex-col items-center justify-center gap-5'>
+        <div className='flex flex-col items-start gap-2'>
+          <p className='font-bold'>오전</p>
+          <div className='mx-auto grid w-full max-w-md grid-cols-2 gap-4 sm:grid-cols-3 md:grid-cols-4'>
+            {morning.map((m) => (
+              <Button
+                key={crypto.randomUUID()}
+                variant={selectedTime === m ? 'default' : 'time'}
+                size='time'
+                onClick={() => setSelectedTime(m)}
+              >
+                {m}
+              </Button>
+            ))}
+          </div>
+        </div>
+        <div className='flex flex-col items-start gap-2'>
+          <p className='font-bold'>오후</p>
+          <div className='mx-auto grid w-full max-w-md grid-cols-2 gap-4 sm:grid-cols-3 md:grid-cols-4'>
+            {afternoon.map((a) => (
+              <Button
+                key={crypto.randomUUID()}
+                variant={selectedTime == a ? 'default' : 'time'}
+                size='time'
+                onClick={() => setSelectedTime(a)}
+              >
+                {a}
+              </Button>
+            ))}
+          </div>
+        </div>
       </CardContent>
-      <CardFooter className='flex justify-evenly'>
+      <CardFooter className='mt-24 flex justify-evenly'>
         <Button onClick={() => onPrev(date)}>이전으로</Button>
         <Button onClick={() => onNext(selectedTime)}>다음으로</Button>
       </CardFooter>
