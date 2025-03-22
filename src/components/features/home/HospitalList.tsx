0c53cf24ac25e4624c0597fd1811708c399d9d49
Minjo Park <consistent.312@gmail.com>
2025-03-22 18:39:41 +0900

    feat:#20 병원 데이터 연결

--- src/components/features/home/HospitalList.tsx
+++ src/components/features/home/HospitalList.tsx
@@ -1,14 +1,16 @@
 import HospitalCard from '@/components/common/HospitalCard';
+import { getAllHospitalData } from '@/utils/api/hospitals';
 
-const HospitalList = () => {
+const HospitalList = async () => {
   // 실제 데이터 적용되면 HospitalCard는 map으로 돌릴 예정입니다.
+  const hospitalList = await getAllHospitalData();
 
   return (
     <div className='border-gray-03 flex flex-[1] flex-col gap-4 border-2 bg-white p-6'>
       <h1 className='text-xl font-bold'>병원 목록</h1>
-      <HospitalCard />
-      <HospitalCard />
-      <HospitalCard />
+      {hospitalList?.map((hospital) => {
+        return <HospitalCard key={hospital.id} hospital={hospital} />;
+      })}
     </div>
   );
 };
