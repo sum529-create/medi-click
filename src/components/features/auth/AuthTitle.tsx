40226b053eef097317f87e4ed5898016b55cf423
sharet9446 <spees97@naver.com>
2025-03-22 22:55:43 +0900

    refactor: AuthTitle 컴포넌트에서 제목을 props로 전달하도록 수정 및 Input 컴포넌트의 날짜 타입에 스타일 추가

--- src/components/features/auth/AuthTitle.tsx
+++ src/components/features/auth/AuthTitle.tsx
@@ -9,7 +9,7 @@ const AuthTitle = ({ children, title }: Props) => {
   return (
     <div className='w-full max-w-md'>
       <div className='rounded-lg bg-white p-8'>
-        <h2 className='mb-8 text-center text-xl font-medium'>로그인</h2>
+        <h2 className='mb-8 text-center text-xl font-medium'>{title}</h2>
         {children}
       </div>
     </div>
