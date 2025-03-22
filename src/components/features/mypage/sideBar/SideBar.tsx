0edeee5634b474f9d7666002583c5ad3a3bede5f
SuimKim <rrrr6563@naver.com>
2025-03-23 00:13:25 +0900

    refactor:#8 불필요한 스타일 class 제거

--- src/components/features/mypage/sideBar/SideBar.tsx
+++ src/components/features/mypage/sideBar/SideBar.tsx
@@ -15,12 +15,8 @@ const SideBar = ({ user }: User) => {
     <aside className='flex flex-col gap-8'>
       <ProfileContainer>
         <ProfileImage />
-        <p className='left-[199px] top-[334px] text-2xl font-bold text-black01'>
-          {name}님
-        </p>
-        <p className='left-[170px] top-[375px] text-base font-bold text-black01'>
-          {email}
-        </p>
+        <p className='text-2xl font-bold text-black01'>{name}님</p>
+        <p className='font-bold text-black01'>{email}</p>
       </ProfileContainer>
       <SideNav />
     </aside>
