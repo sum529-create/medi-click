689a485a81f1050eb0695e5d6de4c780a625c483
SuimKim <rrrr6563@naver.com>
2025-03-23 02:58:55 +0900

    refactor:#32 불필요한 공백 삭제

--- src/components/features/mypage/MyReserveDetail/ReserveContents/ReserveContents.tsx
+++ src/components/features/mypage/MyReserveDetail/ReserveContents/ReserveContents.tsx
@@ -12,12 +12,12 @@ const ReserveContents = () => {
       <InfoContainer>
         <InfoTitleBox />
         <InfoDetailContainer>
-          <InfoDetailContents title='예약자' text='김수임' />
+          <InfoDetailContents title='예약자' text='김수임님' />
           <InfoDetailContents title='진료과' text='정형외과' />
           <InfoDetailContents title='증상' text='다리가 후들거려요.' />
           <Button className='absolute bottom-0 right-8 h-[44px] w-[146px] rounded-[10px] font-bold'>
             리뷰 작성
-          </Button>{' '}
+          </Button>
           {/*날짜가 지나면 해당 버튼이 나타나도록*/}
         </InfoDetailContainer>
       </InfoContainer>
