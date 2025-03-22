06c7003812b763684e41a77b2ffeb63f22b3e960
sharet9446 <spees97@naver.com>
2025-03-22 22:35:07 +0900

    refactor: AuthWhether 컴포넌트를 사용하여 로그인 및 회원가입 페이지의 링크 텍스트 통합

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,20 +1,18 @@
 import Link from 'next/link';
 import AuthForm from '@/components/features/auth/AuthForm';
 import AuthTitle from '@/components/features/auth/AuthTitle';
+import AuthWhether from '@/components/features/auth/AuthWhether';
 import { PATH } from '@/constants/routerPath';
 
 const LoginPage = () => {
   return (
     <AuthTitle title='로그인'>
       <AuthForm mode='login' />
-
-      <div className='mt-4 text-center text-sm'>
-        <span>아직 계정이 없으신가요? </span>
-
+      <AuthWhether whether='아직 계정이 없으신가요? '>
         <Link href={PATH.SIGNUP} className='text-main'>
           회원가입하러가기
         </Link>
-      </div>
+      </AuthWhether>
     </AuthTitle>
   );
 };
--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,20 +1,18 @@
 import Link from 'next/link';
 import AuthForm from '@/components/features/auth/AuthForm';
 import AuthTitle from '@/components/features/auth/AuthTitle';
+import AuthWhether from '@/components/features/auth/AuthWhether';
 import { PATH } from '@/constants/routerPath';
 
 const SignUpPage = () => {
   return (
     <AuthTitle title='회원가입'>
       <AuthForm mode='signup' />
-
-      <div className='mt-4 text-center text-sm'>
-        <span>이미 계정이 있으신가요? </span>
-
+      <AuthWhether whether='이미 계정이 있으신가요? '>
         <Link href={PATH.LOGIN} className='text-main'>
           로그인하러가기
         </Link>
-      </div>
+      </AuthWhether>
     </AuthTitle>
   );
 };
