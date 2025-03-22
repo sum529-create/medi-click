941a1cce2802680e9694cae40d1c5f8d072f4d20
SuimKim <rrrr6563@naver.com>
2025-03-23 04:31:37 +0900

    refactor:#37 변경된 컴포넌트 적용

--- src/components/features/mypage/MyReserveCalendar/ScheduleBox/ScheduleBox.tsx
+++ src/components/features/mypage/MyReserveCalendar/ScheduleBox/ScheduleBox.tsx
@@ -1,27 +1,18 @@
-import { Button } from '@/components/ui/button';
-import ScheduleDetailContainer from '../../ScheduleDetailContainer';
-import ScheduleDetailContents from '../../ScheduleDetailContents';
+import { ScheduleData } from '@/types/components/ui/mypage.schedule';
+import ScheduleDetail from '../../ScheduleDetail';
 import ScheduleContainer from './ScheduleContainer';
 import ScheduleTitleBox from './ScheduleTitleBox';
 
 const ScheduleBox = () => {
+  const testData: ScheduleData = {
+    hospitalName: '서울통정형외과의원 잠실점',
+    schedule: '2025년 3월 20일 (목) 14:00',
+    status: 'ok',
+  };
   return (
     <ScheduleContainer>
       <ScheduleTitleBox />
-      <ScheduleDetailContainer>
-        <ScheduleDetailContents>
-          <div>
-            <h3 className='text-2xl font-bold text-deep-blue'>ㅇㅇ 내과의원</h3>
-            <p className='text-xl font-medium text-black01'>
-              2025년 3월 20일 (목) 14:00
-            </p>
-          </div>
-          <p className='text-lg font-medium text-black01'>상태: 예약 확정</p>
-        </ScheduleDetailContents>
-        <Button className='absolute right-10 top-12 h-[42px] w-[144px] bg-deep-blue text-lg font-bold hover:bg-black01'>
-          상세보기
-        </Button>
-      </ScheduleDetailContainer>
+      <ScheduleDetail scheduleData={testData} />
     </ScheduleContainer>
   );
 };
