31b1f64d93d24c1b20e09dd06a28024adf353dea
sharet9446 <spees97@naver.com>
2025-03-23 02:23:52 +0900

    refactor:#15 AuthForm 및 AuthWhether 컴포넌트를 AuthTitle로 감싸 로그인 및 회원가입 페이지 레이아웃 개선

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,24 +1,19 @@
 import Link from 'next/link';
 import AuthForm from '@/components/features/auth/AuthForm';
+import AuthTitle from '@/components/features/auth/AuthTitle';
+import AuthWhether from '@/components/features/auth/AuthWhether';
 import { PATH } from '@/constants/routerPath';
 
 const LoginPage = () => {
   return (
-    <div className='w-full max-w-md'>
-      <div className='rounded-lg bg-white p-8'>
-        <h2 className='mb-8 text-center text-xl font-medium'>로그인</h2>
-
-        <AuthForm mode='login' />
-
-        <div className='mt-4 text-center text-sm'>
-          <span>아직 계정이 없으신가요? </span>
-
-          <Link href={PATH.SIGNUP} className='text-main'>
-            회원가입하러가기
-          </Link>
-        </div>
-      </div>
-    </div>
+    <AuthTitle title='로그인'>
+      <AuthForm mode='login' />
+      <AuthWhether whether='아직 계정이 없으신가요? '>
+        <Link href={PATH.SIGNUP} className='text-main'>
+          회원가입하러가기
+        </Link>
+      </AuthWhether>
+    </AuthTitle>
   );
 };
 
--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,24 +1,19 @@
 import Link from 'next/link';
 import AuthForm from '@/components/features/auth/AuthForm';
+import AuthTitle from '@/components/features/auth/AuthTitle';
+import AuthWhether from '@/components/features/auth/AuthWhether';
 import { PATH } from '@/constants/routerPath';
 
 const SignUpPage = () => {
   return (
-    <div className='w-full max-w-md'>
-      <div className='rounded-lg bg-white p-8'>
-        <h2 className='mb-8 text-center text-xl font-medium'>회원가입</h2>
-
-        <AuthForm mode='signup' />
-
-        <div className='mt-4 text-center text-sm'>
-          <span>이미 계정이 있으신가요? </span>
-
-          <Link href={PATH.LOGIN} className='text-main'>
-            로그인하러가기
-          </Link>
-        </div>
-      </div>
-    </div>
+    <AuthTitle title='회원가입'>
+      <AuthForm mode='signup' />
+      <AuthWhether whether='이미 계정이 있으신가요? '>
+        <Link href={PATH.LOGIN} className='text-main'>
+          로그인하러가기
+        </Link>
+      </AuthWhether>
+    </AuthTitle>
   );
 };
 
