80f8ebbc23518acfbfa6b872eaa7dbb5dbcc30a8
sharet9446 <spees97@naver.com>
2025-03-22 22:27:52 +0900

    refactor: AuthTitle 컴포넌트를 사용하여 로그인 및 회원가입 페이지 제목 통합

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,24 +1,21 @@
 import Link from 'next/link';
 import AuthForm from '@/components/features/auth/AuthForm';
+import AuthTitle from '@/components/features/auth/AuthTitle';
 import { PATH } from '@/constants/routerPath';
 
 const LoginPage = () => {
   return (
-    <div className='w-full max-w-md'>
-      <div className='rounded-lg bg-white p-8'>
-        <h2 className='mb-8 text-center text-xl font-medium'>로그인</h2>
+    <AuthTitle title='로그인'>
+      <AuthForm mode='login' />
 
-        <AuthForm mode='login' />
+      <div className='mt-4 text-center text-sm'>
+        <span>아직 계정이 없으신가요? </span>
 
-        <div className='mt-4 text-center text-sm'>
-          <span>아직 계정이 없으신가요? </span>
-
-          <Link href={PATH.SIGNUP} className='text-main'>
-            회원가입하러가기
-          </Link>
-        </div>
+        <Link href={PATH.SIGNUP} className='text-main'>
+          회원가입하러가기
+        </Link>
       </div>
-    </div>
+    </AuthTitle>
   );
 };
 
--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,24 +1,21 @@
 import Link from 'next/link';
 import AuthForm from '@/components/features/auth/AuthForm';
+import AuthTitle from '@/components/features/auth/AuthTitle';
 import { PATH } from '@/constants/routerPath';
 
 const SignUpPage = () => {
   return (
-    <div className='w-full max-w-md'>
-      <div className='rounded-lg bg-white p-8'>
-        <h2 className='mb-8 text-center text-xl font-medium'>회원가입</h2>
+    <AuthTitle title='회원가입'>
+      <AuthForm mode='signup' />
 
-        <AuthForm mode='signup' />
+      <div className='mt-4 text-center text-sm'>
+        <span>이미 계정이 있으신가요? </span>
 
-        <div className='mt-4 text-center text-sm'>
-          <span>이미 계정이 있으신가요? </span>
-
-          <Link href={PATH.LOGIN} className='text-main'>
-            로그인하러가기
-          </Link>
-        </div>
+        <Link href={PATH.LOGIN} className='text-main'>
+          로그인하러가기
+        </Link>
       </div>
-    </div>
+    </AuthTitle>
   );
 };
 
