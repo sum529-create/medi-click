529473438815dfc9b6d2d1bece47b5a7fe062b5e
lje00220 <155710708+lje00220@users.noreply.github.com>
2025-03-22 14:42:40 +0900

    feat:#22 날짜를 string으로 변환해주는 유틸 함수 생성

--- src/utils/func/getCalendarDate.ts
+++ src/utils/func/getCalendarDate.ts
@@ -1,9 +1,27 @@
+// 달력에서 선택한 날짜를 0000년 00월 00일 형식으로 변환해주는 함수
+// UI 출력용
+
 export const getCalendarDate = (date: Date | undefined) => {
   if (date === undefined) {
     return '';
   }
+
   const year = date.getFullYear();
   const month = date.getMonth() + 1;
   const day = date.getDate();
+
   return `${year}년 ${month}월 ${day}일`;
+};
+
+// 달력에서 선택한 날짜를 0000-00-00 or 0000-0-00 으로 변환해주는 함수
+// Supabase 저장용
+
+export const getSplitDate = (date: Date) => {
+  const dateArr = [];
+
+  dateArr.push(date.getFullYear());
+  dateArr.push(date.getMonth() + 1);
+  dateArr.push(date.getDate());
+
+  return dateArr.join('-');
 };
