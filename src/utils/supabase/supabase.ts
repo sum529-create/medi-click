9ffcec23cb909c441b7ae4446c97363e5b213a32
Minjo Park <consistent.312@gmail.com>
2025-03-22 17:41:21 +0900

    chore:#20 supabase에 할당

--- src/utils/supabase/supabase.ts
+++ src/utils/supabase/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 import { Database } from '@/types/supabase';
 
-export default createClient<Database>(
+export const supabase = createClient<Database>(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
 );
