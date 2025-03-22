f04fc851d57515a7b0ba998a43b7c63fce13f52e
Minjo Park <consistent.312@gmail.com>
2025-03-22 11:13:02 +0900

    style:#20 검색창 위치 및 w값 max로 수정

--- src/components/features/home/SearchBar.tsx
+++ src/components/features/home/SearchBar.tsx
@@ -8,7 +8,7 @@ const SearchBar = () => {
 
   return (
     <Input
-      className='absolute top-64 min-h-[60px] w-[900px] rounded-full border-gray03 bg-white px-6 text-xl'
+      className='min-h-[60px] max-w-[800px] rounded-full border-gray03 bg-white px-6 text-lg'
       placeholder='진료 보실 과목을 입력해주세요.'
       value={inputValue}
       onChange={(e) => setInputValue(e.target.value)}
