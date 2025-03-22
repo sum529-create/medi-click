a99246b92ed559f59cf341d600a584f23cd8f49f
SuimKim <rrrr6563@naver.com>
2025-03-22 18:40:58 +0900

    feat:#8 에정된 예약 ui 생성

---
+++ src/components/features/mypage/MyReserveCalendar/ScheduleBox.tsx
@@ -0,0 +1,33 @@
+import { Button } from '@/components/ui/button';
+import ScheduleContainer from './ScheduleBox/ScheduleContainer';
+import ScheduleDetailContainer from './ScheduleBox/ScheduleDetailContainer';
+import ScheduleDetailContents from './ScheduleBox/ScheduleDetailContents';
+import ScheduleTitleBox from './ScheduleBox/ScheduleTitleBox';
+
+const ScheduleBox = () => {
+  return (
+    <ScheduleContainer>
+      <ScheduleTitleBox>
+        <h2 className='my-auto ml-10 text-3xl font-bold text-white'>
+          예정된 예약
+        </h2>
+      </ScheduleTitleBox>
+      <ScheduleDetailContainer>
+        <ScheduleDetailContents>
+          <div>
+            <h3 className='text-2xl font-bold text-deep-blue'>ㅇㅇ 내과의원</h3>
+            <p className='text-xl font-medium text-black01'>
+              2025년 3월 20일 (목) 14:00
+            </p>
+          </div>
+          <p className='text-lg font-medium text-black01'>상태: 예약 확정</p>
+        </ScheduleDetailContents>
+        <Button className='absolute right-10 top-12 h-[42px] w-[144px] bg-deep-blue text-lg font-bold hover:bg-black01'>
+          상세보기
+        </Button>
+      </ScheduleDetailContainer>
+    </ScheduleContainer>
+  );
+};
+
+export default ScheduleBox;
