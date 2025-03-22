3b7663cd691d00a9913570b2420354f3269ad408
SuimKim <rrrr6563@naver.com>
2025-03-23 02:48:51 +0900

    feat:#32 예약 정보에 포함될 ui 컴포넌트단으로 분리

---
+++ src/components/features/mypage/MyReserveDetail/ReserveContents/InfoDetailContents.tsx
@@ -0,0 +1,22 @@
+import Text from '@/components/ui/Text';
+import Title from '@/components/ui/Title';
+
+interface DetailContents {
+  title: string;
+  text: string;
+}
+
+const InfoDetailContents = ({ title, text }: DetailContents) => {
+  return (
+    <div className='flex flex-col gap-3'>
+      <Title tag='h3' size='md' align='left' color='black02'>
+        {title}
+      </Title>
+      <Text size='lg' align='left' color='black02'>
+        {text}님
+      </Text>
+    </div>
+  );
+};
+
+export default InfoDetailContents;
---
+++ src/components/features/mypage/MyReserveDetail/ReserveContents/ReserveContents.tsx
@@ -0,0 +1,29 @@
+import { Button } from '@/components/ui/button';
+import ContentsContainer from './ContentsContainer';
+import InfoContainer from './InfoContainer';
+import InfoDetailContainer from './InfoDetailContainer';
+import InfoDetailContents from './InfoDetailContents';
+import InfoMap from './InfoMap';
+import InfoTitleBox from './InfoTitleBox';
+
+const ReserveContents = () => {
+  return (
+    <ContentsContainer>
+      <InfoContainer>
+        <InfoTitleBox />
+        <InfoDetailContainer>
+          <InfoDetailContents title='예약자' text='김수임' />
+          <InfoDetailContents title='진료과' text='정형외과' />
+          <InfoDetailContents title='증상' text='다리가 후들거려요.' />
+          <Button className='absolute bottom-0 right-8 h-[44px] w-[146px] rounded-[10px] font-bold'>
+            리뷰 작성
+          </Button>{' '}
+          {/*날짜가 지나면 해당 버튼이 나타나도록*/}
+        </InfoDetailContainer>
+      </InfoContainer>
+      <InfoMap />
+    </ContentsContainer>
+  );
+};
+
+export default ReserveContents;
