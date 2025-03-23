118a6e14068ebe8efec322b3bc1d2a36eb2204bf
SuimKim <rrrr6563@naver.com>
2025-03-23 19:35:29 +0900

    refactor:#43 각 메뉴 href 설정

--- src/components/features/mypage/layout/sideBar/SideNav.tsx
+++ src/components/features/mypage/layout/sideBar/SideNav.tsx
@@ -1,16 +1,15 @@
+import { PATH } from '@/constants/routerPath';
 import SideNavWrapper from './SideNavWrapper';
 
 const SideNav = () => {
   return (
     <nav className='h-[276px] w-[264px] rounded-[16px] bg-sub'>
-      <div className='flex h-[66px] w-[264px] items-center justify-center rounded-t-[16px] bg-main'>
-        <p className='text-2xl font-bold text-white'>USER MENU</p>
+      <div className='flex h-[66px] w-[264px] items-center justify-center rounded-t-[16px] bg-main text-2xl font-bold text-white'>
+        USER MENU
       </div>
-      <div>
-        <SideNavWrapper>내 예약 캘린더</SideNavWrapper>
-        <SideNavWrapper>내 예약 목록</SideNavWrapper>
-      </div>
-      <SideNavWrapper className='rounded-b-[16px]'>
+      <SideNavWrapper href={PATH.MYPAGE}>내 예약 캘린더</SideNavWrapper>
+      <SideNavWrapper href={PATH.RESERVATIONS}>내 예약 목록</SideNavWrapper>
+      <SideNavWrapper href={PATH.PROFILE} className='rounded-b-[16px]'>
         개인 정보 수정
       </SideNavWrapper>
     </nav>
