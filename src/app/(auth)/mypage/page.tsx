458cd9c5042ec3997a95f9ea71acd8eb5c41ddc0
SuimKim <rrrr6563@naver.com>
2025-03-23 14:31:26 +0900

    feat:#39 생성한 컴포넌트 적용

--- src/app/(auth)/mypage/page.tsx
+++ src/app/(auth)/mypage/page.tsx
@@ -1,12 +1,13 @@
+import EditProfile from '@/components/features/mypage/EditProfile';
 import MyPageContainer from '@/components/features/mypage/MyPageContainer';
 import MyPageContentsContainer from '@/components/features/mypage/MyPageContentsContainer';
-import MyReserveList from '@/components/features/mypage/MyReserveList';
 import SideBar from '@/components/features/mypage/sideBar/SideBar';
 
 const MyPage = () => {
   const userData = {
     name: '김수임',
     email: 'rrrr6563@naver.com',
+    phoneNumber: '010-1234-5678',
   }; //임시데이터
 
   return (
@@ -14,7 +15,8 @@ const MyPage = () => {
       <SideBar user={userData} />
       <MyPageContentsContainer>
         {/* <MyReserveCalendar /> */}
-        <MyReserveList />
+        {/* <MyReserveList /> */}
+        <EditProfile userData={userData} />
       </MyPageContentsContainer>
     </MyPageContainer>
   );
