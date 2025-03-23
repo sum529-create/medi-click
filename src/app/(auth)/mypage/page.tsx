2ac763004ed0cb7226f10e6b017cb4c2974e74b2
SuimKim <rrrr6563@naver.com>
2025-03-23 18:03:35 +0900

    fix:#39 충돌 사항 수정

--- src/app/(auth)/mypage/page.tsx
+++ src/app/(auth)/mypage/page.tsx
@@ -1,13 +1,12 @@
-import EditProfile from '@/components/features/mypage/EditProfile';
 import MyPageContainer from '@/components/features/mypage/MyPageContainer';
 import MyPageContentsContainer from '@/components/features/mypage/MyPageContentsContainer';
+
 import SideBar from '@/components/features/mypage/sideBar/SideBar';
 
 const MyPage = () => {
   const userData = {
     name: '김수임',
     email: 'rrrr6563@naver.com',
-    phoneNumber: '010-1234-5678',
   }; //임시데이터
 
   return (
@@ -15,8 +14,6 @@ const MyPage = () => {
       <SideBar user={userData} />
       <MyPageContentsContainer>
         {/* <MyReserveCalendar /> */}
-        {/* <MyReserveList /> */}
-        <EditProfile userData={userData} />
       </MyPageContentsContainer>
     </MyPageContainer>
   );
