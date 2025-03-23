51ec69a9d4fc24d78c836fc9395fea0d0b093e72
SuimKim <rrrr6563@naver.com>
2025-03-23 19:35:09 +0900

    refactor:#43 사이드 nav Link 컴포넌트로 변경

--- src/components/features/mypage/layout/sideBar/SideNavWrapper.tsx
+++ src/components/features/mypage/layout/sideBar/SideNavWrapper.tsx
@@ -1,16 +1,21 @@
+import Link from 'next/link';
+
 const SideNavWrapper = ({
   children,
   className,
+  href,
 }: {
   children: React.ReactNode;
   className?: string;
+  href: string;
 }) => {
   return (
-    <div
+    <Link
+      href={href}
       className={`${className} flex h-[70px] items-center justify-center text-xl font-bold text-black02 hover:cursor-pointer hover:bg-sub-hover`}
     >
       {children}
-    </div>
+    </Link>
   );
 };
 
