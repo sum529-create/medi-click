2dbbbfad4aa4ef3f361acc9e4cfda08041253d52
SuimKim <rrrr6563@naver.com>
2025-03-23 19:55:10 +0900

    refactor:#43 map을 통한 메뉴 렌더링 방식으로 변경 및 sidebar 컴포넌트에 user 메뉴 컴포넌트 통합

--- src/components/features/mypage/layout/sideBar/SideBar.tsx
+++ src/components/features/mypage/layout/sideBar/SideBar.tsx
@@ -1,6 +1,7 @@
+import { PATH } from '@/constants/routerPath';
 import ProfileImage from '../../ProfileImage';
 import ProfileContainer from './ProfileContainer';
-import SideNav from './SideNav';
+import UserMenuItem from './UserMenuItem';
 
 interface User {
   user: {
@@ -11,6 +12,16 @@ interface User {
 }
 
 const SideBar = ({ user }: User) => {
+  const menuItems = [
+    { label: '내 예약 캘린더', path: PATH.MYPAGE },
+    { label: '내 예약 목록', path: PATH.RESERVATIONS },
+    {
+      label: '개인 정보 수정',
+      path: PATH.PROFILE,
+      className: 'rounded-b-[16px]',
+    },
+  ];
+
   const { name, email, profileImgPath } = user;
   return (
     <aside className='flex flex-col gap-8'>
@@ -19,7 +30,16 @@ const SideBar = ({ user }: User) => {
         <p className='text-2xl font-bold text-black01'>{name}님</p>
         <p className='font-bold text-black01'>{email}</p>
       </ProfileContainer>
-      <SideNav />
+      <nav className='h-[276px] w-[264px] rounded-[16px] bg-sub'>
+        <div className='flex h-[66px] w-[264px] items-center justify-center rounded-t-[16px] bg-main text-2xl font-bold text-white'>
+          USER MENU
+        </div>
+        {menuItems.map((item, index) => (
+          <UserMenuItem key={index} className={item.className} href={item.path}>
+            {item.label}
+          </UserMenuItem>
+        ))}
+      </nav>
     </aside>
   );
 };
