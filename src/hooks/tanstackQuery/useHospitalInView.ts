7a9b4c1367f3dfafea8f0340566e3731405045be
Minjo Park <consistent.312@gmail.com>
2025-03-24 10:33:44 +0900

    docs:#28 주석 추가

--- src/hooks/tanstackQuery/useHospitalInView.ts
+++ src/hooks/tanstackQuery/useHospitalInView.ts
@@ -1,5 +1,8 @@
 import { useInView } from 'react-intersection-observer';
 
+/** 10개의 병원 목록의 끝에 도달했을 때를 인지하고, 다음 페이지를 보여주는 훅
+ * @returns useInView
+ */
 interface UseHospitalInView {
   hasNextPage: boolean | undefined;
   isFetchingNextPage: boolean;
--- src/hooks/tanstackQuery/useHospitalsInfiniteQuery.ts
+++ src/hooks/tanstackQuery/useHospitalsInfiniteQuery.ts
@@ -4,13 +4,21 @@ import { useInfiniteQuery } from '@tanstack/react-query';
 import { QUERY_KEY } from '@/constants/queryKey';
 import { getAllHospitalData } from '@/utils/api/hospitals';
 
+/**
+ * 병원 목록을 10개 단위로 무한 스크롤할 수 있게 해주는 훅
+ * @returns useInfiniteQuery
+ */
 export const useHospitalsInfiniteQuery = () => {
+  const HOSPITAL_PAGE_SIZE = 10;
+
   return useInfiniteQuery({
     queryKey: [QUERY_KEY.HOSPITAL],
     queryFn: ({ pageParam = 1 }) => getAllHospitalData(pageParam),
     initialPageParam: 1,
     getNextPageParam: (lastPage, allPages) => {
-      return lastPage?.length === 10 ? allPages.length + 1 : undefined;
+      return lastPage?.length === HOSPITAL_PAGE_SIZE
+        ? allPages.length + 1
+        : undefined;
     },
   });
 };
