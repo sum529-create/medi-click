4709354deaf9988ebe7212504239b59a06c43a05
Minjo Park <consistent.312@gmail.com>
2025-03-24 20:23:20 +0900

    feat:#52 검색키워드에서 특수문자 및 공백 제거

--- src/components/features/home/SearchBar.tsx
+++ src/components/features/home/SearchBar.tsx
@@ -1,23 +1,28 @@
 'use client';
 
 import { debounce } from 'lodash';
-import { useCallback, useEffect } from 'react';
+import { useCallback, useState } from 'react';
 import { IoSearch } from 'react-icons/io5';
 import { Input } from '@/components/ui/input';
 import { useSearchStore } from '@/utils/zustand/useSearchStore';
 
 const SearchBar = () => {
   const DEBOUNCE_DELAY = 400;
-  const { searchKeyword, setSearchKeyword } = useSearchStore((state) => state);
+  const [inputValue, setInputValue] = useState('');
+  const setSearchKeyword = useSearchStore((state) => state.setSearchKeyword);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value);
+
+    const filteredKeyword = e.target.value.replace(/[^가-힣a-zA-Z0-9]/g, '');
+    setSearchKeyword(filteredKeyword);
+    debouncedSearch(filteredKeyword);
+  };
 
   const debouncedSearch = useCallback(
     debounce(setSearchKeyword, DEBOUNCE_DELAY),
     [],
   );
-
-  useEffect(() => {
-    debouncedSearch(searchKeyword);
-  }, [searchKeyword, debouncedSearch]);
 
   return (
     <div className='relative w-full max-w-[800px]'>
@@ -26,8 +31,8 @@ const SearchBar = () => {
         type='text'
         className='min-h-[60px] rounded-full border-2 border-gray03 bg-white pl-6 pr-14 text-lg'
         placeholder='원하는 병원을 찾아보세요.'
-        value={searchKeyword}
-        onChange={(e) => setSearchKeyword(e.target.value)}
+        value={inputValue}
+        onChange={handleChange}
       />
     </div>
   );
