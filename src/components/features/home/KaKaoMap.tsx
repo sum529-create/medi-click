ea7398d227366efbff8e3a4f4851239402661d76
Minjo Park <consistent.312@gmail.com>
2025-03-24 16:48:26 +0900

    style:#52 카카오 지도 크기 고정

--- src/components/features/home/KaKaoMap.tsx
+++ src/components/features/home/KaKaoMap.tsx
@@ -2,6 +2,7 @@
 
 import { Map, MapMarker } from 'react-kakao-maps-sdk';
 import Loading from '@/components/common/Loading';
+import Text from '@/components/ui/Text';
 import { useAllHospitalLocation } from '@/hooks/map/useAllHospitalLocation';
 import { useCurrentLocation } from '@/hooks/map/useCurrentLocation';
 import { useKakaoLoad } from '@/hooks/map/useKakaoLoad';
@@ -14,15 +15,15 @@ const KaKaoMap = () => {
 
   if (loading)
     return (
-      <div className='flex-[2]'>
+      <div className='h-[300px] flex-[2] border-2 md:h-[750px]'>
         <Loading size={180} />
       </div>
     );
 
   if (error)
     return (
-      <div className='flex flex-[2] items-center justify-center'>
-        지도를 불러오는데 오류가 발생하였습니다.
+      <div className='flex h-[300px] flex-[2] items-center justify-center border-2 md:h-[750px]'>
+        <Text>지도를 불러오는데 오류가 발생하였습니다.</Text>
       </div>
     );
 
@@ -31,7 +32,7 @@ const KaKaoMap = () => {
       <Map
         center={currentLocation}
         level={3}
-        className='h-[300px] w-full md:h-full'
+        className='h-[300px] md:h-[750px]'
       >
         {hospitalLocationList.map((position, idx) => {
           return <MapMarker key={idx} position={position} />;
