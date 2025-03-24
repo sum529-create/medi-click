61f1caf6eb74daa63e864b8b1cd6924efc37fc9e
SuimKim <rrrr6563@naver.com>
2025-03-24 17:01:35 +0900

    refactor:#49 함수 반환 타입 명시

--- src/utils/api/userProfile.ts
+++ src/utils/api/userProfile.ts
@@ -1,11 +1,14 @@
+import { Database } from '@/types/supabase';
 import { supabase } from '../supabase/supabase';
+
+type ProfileData = Database['public']['Tables']['users']['Row'][] | null;
 
 /**
  * supabase에서 현재 로그인 된 유저 정보를 불러오는 api 함수입니다.
  * id는 주스탠드나 로그인 세션에 있을 값을 가져오는 것으로 수정 예정입니다.
- * @returns 현재 로그인 된 유저 정보
+ * @returns { data } 현재 로그인 된 유저 정보
  */
-export const getUserProfile = async () => {
+export const getUserProfile = async (): Promise<ProfileData> => {
   const testId: string = '0d6511b9-926b-443f-9828-39e5f302e1e4'; // zustand 또는 로그인 세션에서 받아올 현재 로그인 된 유저 아이디 값
 
   const { data, error } = await supabase
