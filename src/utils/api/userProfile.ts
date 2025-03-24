c24e5725f80dcedb067eaead9d5b72b288c98f5a
SuimKim <rrrr6563@naver.com>
2025-03-24 19:27:37 +0900

    feat:#49 회원 정보 수정하는 api 함수 구현

--- src/utils/api/userProfile.ts
+++ src/utils/api/userProfile.ts
@@ -2,19 +2,19 @@
 import { Database } from '@/types/supabase';
 import { supabase } from '../supabase/supabase';
 
+const testId: string = '0d6511b9-926b-443f-9828-39e5f302e1e4'; // zustand 또는 로그인 세션에서 받아올 현재 로그인 된 유저 아이디 값
+
 type ProfileData = Database['public']['Tables']['users']['Row'][] | null;
 // interface SessionData {
 //   session: Session | null;
 // }
 
 /**
- * supabase에서 현재 로그인 된 유저 정보를 불러오는 api 함수입니다.
+ * supabase에서 현재 로그인 된 사용자의 회원 정보를 불러오는 api 함수입니다.
  * id는 주스탠드나 로그인 세션에 있을 값을 가져오는 것으로 수정 예정입니다.
  * @returns { data } 현재 로그인 된 유저 정보
  */
 export const getUserProfile = async (): Promise<ProfileData> => {
-  const testId: string = '0d6511b9-926b-443f-9828-39e5f302e1e4'; // zustand 또는 로그인 세션에서 받아올 현재 로그인 된 유저 아이디 값
-
   const { data, error } = await supabase
     .from('users')
     .select('*')
@@ -33,3 +33,17 @@ export const getUserProfile = async (): Promise<ProfileData> => {
 
 //   return data;
 // };
+
+/**
+ * 로그인 된 사용자의 회원 정보를 수정하는 api 함수입니다.
+ * id는 주스탠드나 로그인 세션에 있을 값을 가져오는 것으로 수정 예정입니다.
+ */
+export const updateUserProfile = async (
+  updatedPhoneNumber: string,
+): Promise<void> => {
+  const { error } = await supabase
+    .from('users')
+    .update({ phone_number: updatedPhoneNumber })
+    .eq('id', testId);
+  if (error) console.log('error', error);
+};
