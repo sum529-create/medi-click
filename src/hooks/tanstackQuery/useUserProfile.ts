ca3d24fd09ec7a2f176994ed2155f7f45c0eabaf
SuimKim <rrrr6563@naver.com>
2025-03-25 02:44:48 +0900

    refactor:#49 데이터 받아올 때 single()로 처리

--- src/hooks/tanstackQuery/useUserProfile.ts
+++ src/hooks/tanstackQuery/useUserProfile.ts
@@ -22,16 +22,15 @@ interface UseUserProfileReturnValue {
 
 /**
  * 현재 로그인 된 사용자의 회원 정보를 불러오는 훅입니다.
- * useQuery 훅과 getUserProfile api 함수를 사용하여 데이터를 받아온 후 첫번째 인덱스에 있는 데이터를 반환합니다.
- * 유저 정보는 user id당 하나만 존재하기 때문에 하나의 인덱스만 존재하기에 첫번째 인덱스를 반환하는 것으로 처리했습니다.
+ * useQuery 훅과 getUserProfile api 함수를 사용하여 데이터를 받아온 후 반환합니다.
  * profileData가 존재하지 않으면 early return 후  { isProfileError, isProfilePending, getProfileError }를 반환합니다.
- * 그렇지 않은 경우엔 profileData가 보장된 상태이므로 profileData[0]을 반환합니다.
+ * 그렇지 않은 경우엔 profile 보장된 상태이므로 profile을 반환합니다.
  *
  * @returns { isProfilePending, isProfileError, getProfileError, profile }
  */
 export const useUserProfile = (): Partial<UseUserProfileReturnValue> => {
   const {
-    data: profileData,
+    data: profile,
     isError: isProfileError,
     isPending: isProfilePending,
     error: getProfileError,
@@ -40,10 +39,7 @@ export const useUserProfile = (): Partial<UseUserProfileReturnValue> => {
     queryFn: getUserProfile,
   });
 
-  if (!profileData)
-    return { isProfileError, isProfilePending, getProfileError };
-
-  const profile = profileData[0];
+  if (!profile) return { isProfileError, isProfilePending, getProfileError };
 
   return { profile };
 };
--- src/utils/api/userProfile.ts
+++ src/utils/api/userProfile.ts
@@ -5,7 +5,7 @@ import { supabase } from '../supabase/supabase';
 
 const testId: string = '0d6511b9-926b-443f-9828-39e5f302e1e4'; // zustand 또는 로그인 세션에서 받아올 현재 로그인 된 유저 아이디 값
 
-type ProfileData = Database['public']['Tables']['users']['Row'][] | null;
+type ProfileData = Database['public']['Tables']['users']['Row'] | null;
 
 /**
  * supabase에서 현재 로그인 된 사용자의 회원 정보를 불러오는 api 함수입니다.
@@ -16,7 +16,8 @@ export const getUserProfile = async (): Promise<ProfileData> => {
   const { data, error } = await supabase
     .from(TABLE.USERS)
     .select('*')
-    .eq(COLUMN.ID, testId);
+    .eq(COLUMN.ID, testId)
+    .single();
   if (error) console.log('유저 프로필 받아오기 실패', error);
 
   return data;
