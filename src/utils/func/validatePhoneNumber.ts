605af6395a5196a8485427eacd7b7f75246e16ef
SuimKim <rrrr6563@naver.com>
2025-03-25 03:36:41 +0900

    refactor:#49 유효성 검사 고도화

--- src/utils/func/validatePhoneNumber.ts
+++ src/utils/func/validatePhoneNumber.ts
@@ -1,3 +1,36 @@
-export const validatePhoneNumber = (phoneNumber: string): boolean => {
-  return /^010-\d{4}-\d{4}$/.test(phoneNumber);
+interface ReturnValue {
+  formatCheck: boolean;
+  changeCheck: boolean;
+  errorMessage: string | null;
+}
+
+export const validatePhoneNumber = (
+  nowPhoneNumber: string,
+  updatedPhoneNumber: string,
+): ReturnValue => {
+  const formatCheck: boolean = /^010-\d{4}-\d{4}$/.test(updatedPhoneNumber);
+  const changeCheck: boolean = nowPhoneNumber !== updatedPhoneNumber;
+  let errorMessage: string | null = null;
+
+  if (changeCheck && formatCheck) {
+    errorMessage = null;
+  }
+
+  if (!formatCheck) {
+    errorMessage = '010-XXXX-XXXX 형식으로 입력해주세요.';
+  }
+
+  if (!formatCheck && !changeCheck) {
+    errorMessage = '010-XXXX-XXXX 형식으로 입력해주세요.';
+  }
+
+  if (formatCheck && !changeCheck) {
+    errorMessage = '현재 연락처와 일치합니다.';
+  }
+
+  if (!changeCheck) {
+    errorMessage = '현재 연락처와 일치합니다.';
+  }
+
+  return { formatCheck, changeCheck, errorMessage };
 };
