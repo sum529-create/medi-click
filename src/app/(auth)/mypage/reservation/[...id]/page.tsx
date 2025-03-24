798f3bf01c439846d0020a5b44ec7cf9df632480
SuimKim <rrrr6563@naver.com>
2025-03-25 05:31:15 +0900

    feat:#69 pathId로 선택한 예약 가져오기

--- src/app/(auth)/mypage/reservation/[...id]/page.tsx
+++ src/app/(auth)/mypage/reservation/[...id]/page.tsx
@@ -1,11 +1,28 @@
+'use client';
+
+import Loading from '@/components/common/Loading';
 import Banner from '@/components/features/mypage/myReserveDetail/banner/Banner';
 import ReserveContents from '@/components/features/mypage/myReserveDetail/reserveContents/ReserveContents';
+import { QUERY_KEY } from '@/constants/queryKey';
+import { useMyPageData } from '@/hooks/tanstackQuery/useMyPageData';
+import { getReservationDetail } from '@/utils/api/reservation';
 
 const ReservationDetailPage = ({ params }: { params: { id: string } }) => {
+  const pathId = params.id;
+
+  const { isError, isPending, error, data } = useMyPageData(
+    QUERY_KEY.RESERVATION,
+    () => getReservationDetail(pathId),
+  );
+
+  if (isError) throw error;
+  if (isPending) return <Loading size={30} />;
+  if (!data) return;
+
   return (
     <>
-      <Banner />
-      <ReserveContents />
+      <Banner reservation={data} />
+      <ReserveContents reservation={data} />
     </>
   );
 };
