fbf14102898dff641e63bce39ee9806ce8eef904
Minjo Park <consistent.312@gmail.com>
2025-03-24 14:47:20 +0900

    style:#52 검색 아이콘 뒤로 텍스트가 입력되는 문제 해결

--- src/components/features/home/SearchBar.tsx
+++ src/components/features/home/SearchBar.tsx
@@ -11,8 +11,9 @@ const SearchBar = () => {
     <div className='relative w-full max-w-[800px]'>
       <IoSearch className='text-gray0 absolute right-4 top-1/2 -translate-y-1/2 text-3xl text-gray-400' />
       <Input
-        className='min-h-[60px] rounded-full border-2 border-gray03 bg-white px-6 text-lg'
-        placeholder='진료 보실 과목을 입력해주세요.'
+        type='text'
+        className='min-h-[60px] rounded-full border-2 border-gray03 bg-white pl-6 pr-14 text-lg'
+        placeholder='어떤 병원을 예약하실건가요?'
         value={inputValue}
         onChange={(e) => setInputValue(e.target.value)}
       />
