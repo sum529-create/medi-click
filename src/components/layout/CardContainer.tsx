6219e0ace38e5471ddbfaf9881aa37c35e02ae2b
lje00220 <155710708+lje00220@users.noreply.github.com>
2025-03-25 01:40:54 +0900

    style:#59 카드 컨테이너 크기 고정

--- src/components/layout/CardContainer.tsx
+++ src/components/layout/CardContainer.tsx
@@ -2,8 +2,8 @@ import { Card } from '../ui/card';
 
 const CardContainer = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className='flex h-screen w-full justify-center bg-gray02 pt-10'>
-      <Card className='max-h-fit min-h-[600px] w-[650px] rounded-lg p-6 shadow-lg'>
+    <div className='flex max-h-fit min-h-screen w-full justify-center bg-gray02 py-10'>
+      <Card className='h-[700px] min-h-fit w-[650px] rounded-lg p-6 shadow-lg'>
         {children}
       </Card>
     </div>
