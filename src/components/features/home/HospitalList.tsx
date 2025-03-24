b19e97ce1120aadf8d78749b878a2c6a52f44b0a
Minjo Park <consistent.312@gmail.com>
2025-03-24 16:37:06 +0900

    fix:#52 검색한 키워드가 없을 때를 처리하는 조건부 렌더링 추가

--- src/components/features/home/HospitalList.tsx
+++ src/components/features/home/HospitalList.tsx
@@ -2,6 +2,7 @@
 
 import HospitalCard from '@/components/common/HospitalCard';
 import Loading from '@/components/common/Loading';
+import Text from '@/components/ui/Text';
 import Title from '@/components/ui/Title';
 import { useHospitalInView } from '@/hooks/tanstackQuery/useHospitalInView';
 import { useHospitalsInfiniteQuery } from '@/hooks/tanstackQuery/useHospitalsInfiniteQuery';
@@ -21,22 +22,32 @@ const HospitalList = () => {
     fetchNextPage,
   });
 
-  if (status === 'pending') return <Loading size={100} />;
+  if (status === 'pending')
+    return (
+      <div className='flex-[1]'>
+        <Loading size={100} />
+      </div>
+    );
   if (status === 'error')
     return (
-      <div className='flex items-center justify-center'>
-        병원 목록을 불러오는데 오류가 발생하였습니다.
+      <div className='flex flex-[1] items-center justify-center'>
+        <Text>병원 목록을 불러오는데 오류가 발생하였습니다.</Text>
       </div>
     );
 
   return (
-    <div className='border-gray-03 flex max-h-[750px] min-h-[350px] flex-[1] flex-col gap-4 overflow-y-auto border-2 bg-white p-6'>
+    <div className='border-gray-03 flex max-h-[750px] min-h-[400px] flex-[1] flex-col gap-4 overflow-y-auto border-2 bg-white p-6'>
       <Title>병원 목록</Title>
-      {hospitalList?.pages.map((page) =>
-        page.map((hospital) => {
-          return <HospitalCard key={hospital.id} hospital={hospital} />;
-        }),
+      {!hospitalList || hospitalList.pages.flat().length === 0 ? (
+        <Text>검색하신 병원이 존재하지 않습니다.</Text>
+      ) : (
+        hospitalList.pages.map((page) =>
+          page.map((hospital) => (
+            <HospitalCard key={hospital.id} hospital={hospital} />
+          )),
+        )
       )}
+
       <div ref={ref}></div>
     </div>
   );
