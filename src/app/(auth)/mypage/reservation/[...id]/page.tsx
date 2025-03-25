662da00f32399832f6933f8194f780b0f10d8701
SuimKim <rrrr6563@naver.com>
2025-03-25 16:24:43 +0900

    refactor:#69 CSR->SSG 변경

--- src/app/(auth)/mypage/reservation/[...id]/page.tsx
+++ src/app/(auth)/mypage/reservation/[...id]/page.tsx
@@ -1,28 +1,20 @@
-'use client';
-
-import Loading from '@/components/common/Loading';
 import Banner from '@/components/features/mypage/myReserveDetail/banner/Banner';
 import ReserveContents from '@/components/features/mypage/myReserveDetail/reserveContents/ReserveContents';
-import { QUERY_KEY } from '@/constants/queryKey';
-import { useMyPageDataQuery } from '@/hooks/tanstackQuery/useMyPageDataQuery';
 import { getReservationDetail } from '@/utils/api/reservation';
 
-const ReservationDetailPage = ({ params }: { params: { id: string } }) => {
+interface Params {
+  params: { id: string };
+}
+
+const ReservationDetailPage = async ({ params }: Params) => {
   const pathId = params.id;
 
-  const { isError, isPending, error, data } = useMyPageDataQuery(
-    QUERY_KEY.RESERVATION,
-    () => getReservationDetail(pathId),
-  );
-
-  if (isError) throw error;
-  if (isPending) return <Loading size={30} />;
-  if (!data) return;
+  const reservationDetailData = await getReservationDetail(pathId);
 
   return (
     <>
-      <Banner reservation={data} />
-      <ReserveContents reservation={data} />
+      <Banner reservation={reservationDetailData} />
+      <ReserveContents reservation={reservationDetailData} />
     </>
   );
 };
