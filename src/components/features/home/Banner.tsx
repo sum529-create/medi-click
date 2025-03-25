25a2739e9ee6e268ceb4604b0b9f652d0ab5a61f
Minjo Park <consistent.312@gmail.com>
2025-03-25 19:46:52 +0900

    style:#65 배너이미지 추가

--- src/components/features/home/Banner.tsx
+++ src/components/features/home/Banner.tsx
@@ -1,13 +1,25 @@
+import Image from 'next/image';
 import Text from '@/components/ui/Text';
 import Title from '@/components/ui/Title';
 
 const Banner = () => {
   return (
-    <div className='flex w-full flex-col justify-center gap-4 rounded-xl bg-sub p-6 md:min-h-[280px] md:p-20'>
-      <Title size='xl' color='deep-blue'>
-        원하는 병원을 클릭 한 번으로
-      </Title>
-      <Text size='xl'>지금 바로 내가 원하는 병원을 찾고 예약해보세요.</Text>
+    <div className='relative flex w-full flex-col overflow-hidden rounded-xl bg-sub p-6 md:min-h-[400px] md:p-20'>
+      <Image
+        src='/img/bannerImg.jpg'
+        fill
+        className='object-cover'
+        alt='배너이미지'
+      />
+      <div className='absolute inset-0 bg-black opacity-60'></div>
+      <div className='z-10 flex flex-col gap-3'>
+        <Title size='xl' color='gray03'>
+          원하는 병원을 클릭 한 번으로
+        </Title>
+        <Text size='xl' color='gray03'>
+          지금 바로 내가 원하는 병원을 찾고 예약해보세요.
+        </Text>
+      </div>
     </div>
   );
 };
