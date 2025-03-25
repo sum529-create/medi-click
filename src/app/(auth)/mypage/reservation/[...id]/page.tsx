85b9c9063f89d7c871b781ad1ae7651026d9794f
SuimKim <rrrr6563@naver.com>
2025-03-25 12:52:08 +0900

    refactor:#69 변경된 훅 이름으로 적용

--- src/app/(auth)/mypage/reservation/[...id]/page.tsx
+++ src/app/(auth)/mypage/reservation/[...id]/page.tsx
@@ -4,13 +4,13 @@ import Loading from '@/components/common/Loading';
 import Banner from '@/components/features/mypage/myReserveDetail/banner/Banner';
 import ReserveContents from '@/components/features/mypage/myReserveDetail/reserveContents/ReserveContents';
 import { QUERY_KEY } from '@/constants/queryKey';
-import { useMyPageData } from '@/hooks/tanstackQuery/useMyPageData';
+import { useMyPageDataQuery } from '@/hooks/tanstackQuery/useMyPageDataQuery';
 import { getReservationDetail } from '@/utils/api/reservation';
 
 const ReservationDetailPage = ({ params }: { params: { id: string } }) => {
   const pathId = params.id;
 
-  const { isError, isPending, error, data } = useMyPageData(
+  const { isError, isPending, error, data } = useMyPageDataQuery(
     QUERY_KEY.RESERVATION,
     () => getReservationDetail(pathId),
   );
