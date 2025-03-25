80d3f54827842aa7b72b1bfb849af4c2e29ee238
SuimKim <rrrr6563@naver.com>
2025-03-25 12:10:52 +0900

    style:#67 title 컴포넌트 색상 추가 및 적용

--- src/components/features/mypage/MainContentsTitleBox.tsx
+++ src/components/features/mypage/MainContentsTitleBox.tsx
@@ -1,7 +1,11 @@
+import Title from '@/components/ui/Title';
+
 const MainContentsTitleBox = ({ title }: { title: string }) => {
   return (
-    <div className='flex h-[90px] items-center rounded-[13px] bg-deep-blue pl-10 text-3xl font-bold text-white'>
-      {title}
+    <div className='flex h-[90px] items-center rounded-[13px] bg-deep-blue pl-10'>
+      <Title tag='h2' size='lg' color='white'>
+        {title}
+      </Title>
     </div>
   );
 };
