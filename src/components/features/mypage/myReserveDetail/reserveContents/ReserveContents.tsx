b12ff00c525f08e2d0fff872ed0a28e942affeab
SuimKim <rrrr6563@naver.com>
2025-03-25 16:43:41 +0900

    refactor:#69 병원 상세정보 받아와서 출력

--- src/components/features/mypage/myReserveDetail/reserveContents/ReserveContents.tsx
+++ src/components/features/mypage/myReserveDetail/reserveContents/ReserveContents.tsx
@@ -1,5 +1,6 @@
 import { Button } from '@/components/ui/button';
 import { ReservationProps } from '@/types/components/mypage/reservation.type';
+import hospitalDetail from '@/utils/api/hospitalDetail';
 import ContentsContainer from './ContentsContainer';
 import InfoContainer from './InfoContainer';
 import InfoDetailContainer from './InfoDetailContainer';
@@ -7,17 +8,20 @@ import InfoDetailContents from './InfoDetailContents';
 import InfoMap from './InfoMap';
 import InfoTitleBox from './InfoTitleBox';
 
-const ReserveContents = ({ reservation }: ReservationProps) => {
-  console.log('reservation', reservation);
+const ReserveContents = async ({ reservation }: ReservationProps) => {
+  const { dgidIdName } = await hospitalDetail(reservation.hospital_id);
+  console.log('dgidIdName', dgidIdName);
+
   return (
     <ContentsContainer>
       <InfoContainer>
         <InfoTitleBox />
         <InfoDetailContainer>
+          {/* 유저정보에서 받아올 이름 */}
           <InfoDetailContents title='예약자' text='김수임님' />
-          <InfoDetailContents title='진료과' text='정형외과' />
+          <InfoDetailContents title='진료과' text={dgidIdName} />
           <InfoDetailContents title='증상' text={reservation.memo} />
-          <Button className='absolute bottom-0 right-8 h-[44px] w-[146px] rounded-[10px] font-bold'>
+          <Button className='absolute right-8 top-48 h-[44px] w-[146px] rounded-[10px] font-bold'>
             리뷰 작성
           </Button>
           {/*날짜가 지나면 해당 버튼이 나타나도록*/}
