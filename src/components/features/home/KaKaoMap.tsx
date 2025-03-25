793952784ef11359b170532b61833bdfc4241b2d
Minjo Park <consistent.312@gmail.com>
2025-03-25 22:47:54 +0900

    fix:#65 지도 움직이고 나서 현재 위치 버튼 누르면 돌아오지 않는 문제 해결

--- src/components/features/home/KaKaoMap.tsx
+++ src/components/features/home/KaKaoMap.tsx
@@ -1,6 +1,7 @@
 'use client';
 
-import { useEffect, useState } from 'react';
+import { debounce } from 'lodash-es';
+import { useEffect, useMemo, useState } from 'react';
 import { Map } from 'react-kakao-maps-sdk';
 import Loading from '@/components/common/Loading';
 import Text from '@/components/ui/Text';
@@ -14,6 +15,9 @@ import EventMarkerContainer from './map/EventMarkerContainer';
 import ZoomButton from './map/ZoomButton';
 
 const KaKaoMap = () => {
+  /** constant */
+  const DEBOUNCE_DELAY = 500;
+
   /** custom hook */
   const currentLocation = useCurrentLocation();
   const hospitalLocationList = useAllHospitalLocation();
@@ -37,6 +41,18 @@ const KaKaoMap = () => {
     setMapCenter(currentLocation);
   }, [selectedHospital, currentLocation]);
 
+  // 지도를 움직일 때마다 해당 위치의 중심으로 좌표를 변경하는 함수
+  const updateCenterWhenMapMoved = useMemo(
+    () =>
+      debounce((map: kakao.maps.Map) => {
+        setMapCenter({
+          lat: map.getCenter().getLat(),
+          lng: map.getCenter().getLng(),
+        });
+      }, DEBOUNCE_DELAY),
+    [],
+  );
+
   /** UI */
   if (loading)
     return (
@@ -59,6 +75,7 @@ const KaKaoMap = () => {
         center={mapCenter}
         level={mapLevel}
         ref={mapRef}
+        onCenterChanged={updateCenterWhenMapMoved}
         className='relative h-[300px] md:h-[750px]'
       >
         {/** 마커 */}
@@ -70,7 +87,7 @@ const KaKaoMap = () => {
             setActiveMarkerId={setActiveMarkerId}
           />
         ))}
-        {/** 줌 인/아웃 버튼 */}
+        {/** 줌 인/아웃 버튼 + 현재 위치 버튼 */}
         <div className='absolute right-4 top-4 z-10 flex flex-col gap-4'>
           <ZoomButton zoomControls={{ zoomIn, zoomOut }} />
           <CurrentLocationButton
