07cd33ffeb6b9ee1fe54495676f5f2cfa9ac5111
SuimKim <rrrr6563@naver.com>
2025-03-25 20:22:08 +0900

    test:#69 데이터 없는 상태이므로 주석처리

--- src/app/(auth)/mypage/page.tsx
+++ src/app/(auth)/mypage/page.tsx
@@ -1,11 +1,10 @@
 import MyCalendar from '@/components/features/mypage/myReserveCalendar/myCalendar/MyCalendar';
-import ScheduleBox from '@/components/features/mypage/myReserveCalendar/scheduleBox/ScheduleBox';
 
 const MyPage = () => {
   return (
     <>
       <MyCalendar />
-      <ScheduleBox />
+      {/* <ScheduleBox /> */}
     </>
   );
 };
