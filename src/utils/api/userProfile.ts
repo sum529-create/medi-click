573d82e291ec6674c6a3643e2359fa76b7e882f6
SuimKim <rrrr6563@naver.com>
2025-03-26 14:59:25 +0900

    refactor:#83 타입->인터페이스로 수정

--- src/utils/api/userProfile.ts
+++ src/utils/api/userProfile.ts
@@ -5,14 +5,16 @@ import { supabase } from '../supabase/supabaseClient';
 
 const testId: string = '0d6511b9-926b-443f-9828-39e5f302e1e4'; // zustand 또는 로그인 세션에서 받아올 현재 로그인 된 유저 아이디 값
 
-type ProfileData = Tables<'users'>;
+interface ProfileData {
+  profile: Tables<'users'>;
+}
 
 /**
  * supabase에서 현재 로그인 된 사용자의 회원 정보를 불러오는 api 함수입니다.
  * id는 주스탠드나 로그인 세션에 있을 값을 가져오는 것으로 수정 예정입니다.
  * @returns { data } 현재 로그인 된 유저 정보
  */
-export const getUserProfile = async (): Promise<ProfileData> => {
+export const getUserProfile = async (): Promise<ProfileData['profile']> => {
   const { data, error } = await supabase
     .from(TABLE.USERS)
     .select('*')
