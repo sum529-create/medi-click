1564c24b97b5b030cc29c3af0cb4d26dc2c6a466
SuimKim <rrrr6563@naver.com>
2025-03-26 20:56:35 +0900

    refactor:#83 예약 리스트 받아오는 방식 변경 - 서버에서 받아온 session id를 useMyPageDataQuery 훅에 파라미터로 넣어서 렌더링

--- src/app/(auth)/mypage/reservations/page.tsx
+++ src/app/(auth)/mypage/reservations/page.tsx
@@ -1,20 +1,20 @@
-import ReservationsPage from '@/components/features/mypage/hospitalReservations/Reservations';
 import MainContentsContainer from '@/components/features/mypage/MainContentsContainer';
 import MainContentsTitleBox from '@/components/features/mypage/MainContentsTitleBox';
 import ReservationList from '@/components/features/mypage/myReservations/ReservationList';
 
-const ReserveListPage = () => {
+import { createClient } from '@/utils/supabase/supabaseServer';
+
+const ReserveListPage = async () => {
+  const supabase = createClient();
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
   return (
-    <>
-      {true ? (
-        <MainContentsContainer>
-          <MainContentsTitleBox title='내 예약 목록' />
-          <ReservationList />
-        </MainContentsContainer>
-      ) : (
-        <ReservationsPage />
-      )}
-    </>
+    <MainContentsContainer>
+      <MainContentsTitleBox title='내 예약 목록' />
+      <ReservationList userId={session?.user.id} />
+    </MainContentsContainer>
   );
 };
 
