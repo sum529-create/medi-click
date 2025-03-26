178e9bda678f3e0328b823cedb1f2ac7294d6502
lje00220 <155710708+lje00220@users.noreply.github.com>
2025-03-26 17:08:44 +0900

    fix: vercel 배포 주소 추가

--- src/utils/api/hospitalDetail.ts
+++ src/utils/api/hospitalDetail.ts
@@ -15,7 +15,7 @@ export const getHospitalApiUrl = (id: string) => {
   } else {
     // 서버에서는 절대 URL 사용
     const baseUrl = process.env.VERCEL_URL
-      ? `https://${process.env.VERCEL_URL}`
+      ? process.env.NEXT_PUBLIC_SITE_URL || `https://${process.env.VERCEL_URL}`
       : 'http://localhost:3000';
 
     return `${baseUrl}/api/hospitalDetail?id=${id}`;
