662445dc5d78aa25d57d54bead27e9e272796c37
SuimKim <rrrr6563@naver.com>
2025-03-26 15:22:56 +0900

    feat:#83 예약 정보 store 생성

--- src/store/mypageStore.ts
+++ src/store/mypageStore.ts
@@ -1,5 +1,18 @@
 import { create } from 'zustand';
+import { ReservationProps } from '@/types/components/mypage/reservation.type';
 
+interface ReservationState {
+  reservation: Partial<ReservationProps['reservation']>;
+  setReservation: (reservation: ReservationProps['reservation']) => void; // 전체 예약 설정
+  clearReservation: () => void; // 상태 초기화
+}
+
+export const reservationStore = create<ReservationState>((set) => ({
+  reservation: {}, // 초기값을 null로 설정 (빈 상태)
+  setReservation: (reservation: ReservationProps['reservation']) =>
+    set({ reservation }), // reservation 객체 전체 업데이트
+  clearReservation: () => set({ reservation: {} }), // 초기화
+}));
 interface ShowModalState {
   isShowModal: boolean;
   setIsShowModal: (isShowModal: boolean) => void;
