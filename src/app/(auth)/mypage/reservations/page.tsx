767843d46299de21477626834ce6bd995e3b801d
노수민 <nosumin29@gmail.com>
2025-03-26 15:36:36 +0900

    feat:#86 클라이언트 페이지 분리

--- src/app/(auth)/mypage/reservations/page.tsx
+++ src/app/(auth)/mypage/reservations/page.tsx
@@ -1,21 +1,7 @@
-import ReservationsPage from '@/components/features/mypage/hospitalReservations/Reservations';
-import MainContentsContainer from '@/components/features/mypage/MainContentsContainer';
-import MainContentsTitleBox from '@/components/features/mypage/MainContentsTitleBox';
-import ReservationList from '@/components/features/mypage/myReservations/ReservationList';
+import ReserveListClientPage from './page.client';
 
 const ReserveListPage = () => {
-  return (
-    <>
-      {true ? (
-        <MainContentsContainer>
-          <MainContentsTitleBox title='내 예약 목록' />
-          <ReservationList />
-        </MainContentsContainer>
-      ) : (
-        <ReservationsPage />
-      )}
-    </>
-  );
+  return <ReserveListClientPage />;
 };
 
 export default ReserveListPage;
