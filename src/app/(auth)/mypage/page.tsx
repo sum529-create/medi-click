c4bfe85161e6aba183fd21ebfe9d5ba3062e1d60
sharet9446 <spees97@naver.com>
2025-03-26 20:46:49 +0900

    feat:#89 checkAuthServer 함수에서 리다이렉트 경로 인자 제거 및 인증 확인 로직 되돌림

--- src/app/(auth)/mypage/page.tsx
+++ src/app/(auth)/mypage/page.tsx
@@ -1,9 +1,13 @@
+import { redirect } from 'next/navigation';
 import MyCalendar from '@/components/features/mypage/myReserveCalendar/myCalendar/MyCalendar';
 import { PATH } from '@/constants/routerPath';
 import { checkAuthServer } from '@/utils/access/protectedRouteServer';
 
 const MyPage = async () => {
-  await checkAuthServer({ path: PATH.LOGIN });
+  const isCheckAuth = await checkAuthServer();
+  if (!isCheckAuth) {
+    redirect(PATH.LOGIN);
+  }
 
   return (
     <>
--- src/utils/access/protectedRouteServer.ts
+++ src/utils/access/protectedRouteServer.ts
@@ -1,15 +1,11 @@
-import { redirect } from 'next/navigation';
 import { createClient } from '../supabase/supabaseServer';
 
-interface Props {
-  path: string;
-}
-
-export const checkAuthServer = async ({ path }: Props) => {
+export const checkAuthServer = async () => {
   const supabase = await createClient();
   const { data, error } = await supabase.auth.getUser();
 
   if (error || !data?.user) {
-    redirect(path);
+    return false;
   }
+  return true;
 };
