8eca4be576bf2faecf71f3cb8901b05bfbfdc286
SuimKim <rrrr6563@naver.com>
2025-03-27 03:59:29 +0900

    feat:#105 리뷰 등록 구현

---
+++ src/utils/api/review.ts
@@ -0,0 +1,24 @@
+import { TABLE } from '@/constants/supabaseTables';
+import { Tables } from '@/types/supabase';
+import { supabase } from '../supabase/supabaseClient';
+
+export interface Review {
+  review: Tables<'reviews'>;
+}
+
+export const createReview = async (
+  review: Omit<Review['review'], 'id' | 'created_at'>,
+): Promise<Review['review']> => {
+  const { data, error } = await supabase
+    .from(TABLE.REVIEWS)
+    .insert(review)
+    .select()
+    .single();
+
+  if (error) {
+    console.error('Create review error:', error);
+    throw new Error(`Failed to create review: ${error.message}`);
+  }
+
+  return data as Review['review'];
+};
